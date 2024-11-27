018384d14e18ded8b3784ac9cbcd534d2e8ab984
ztxtech <1665186372@qq.com>
2024-11-27 11:19:53 +0800

    恢复误删方法

--- models/ts2vec/fsnet.py
+++ models/ts2vec/fsnet.py
@@ -88,6 +88,41 @@ class TSEncoder(nn.Module):
 
         return x
 
+    def forward_time(self, x, mask=None):  # x: B x T x input_dims
+        x = x.transpose(1, 2)
+        nan_mask = ~x.isnan().any(axis=-1)
+        x[~nan_mask] = 0
+        x = self.input_fc(x)  # B x T x Ch
+
+        # generate & apply mask
+        if mask is None:
+            if self.training:
+                mask = self.mask_mode
+            else:
+                mask = 'all_true'
+
+        if mask == 'binomial':
+            mask = generate_binomial_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == 'continuous':
+            mask = generate_continuous_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == 'all_true':
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+        elif mask == 'all_false':
+            mask = x.new_full((x.size(0), x.size(1)), False, dtype=torch.bool)
+        elif mask == 'mask_last':
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+            mask[:, -1] = False
+
+        mask &= nan_mask
+        x[~mask] = 0
+
+        # conv encoder
+        x = x.transpose(1, 2)  # B x Ch x T
+        x = self.repr_dropout(self.feature_extractor(x))  # B x Co x T
+        x = x.transpose(1, 2)  # B x T x Co
+
+        return x
+
 
 class BandedFourierLayer(nn.Module):
 
